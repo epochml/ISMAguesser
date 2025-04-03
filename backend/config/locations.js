ec838b7f8c43b20ef4e5b2bf6aabd8382b23533c
nekiwo <58634064+nekiwo@users.noreply.github.com>
2025-04-02 22:31:05 -0500

    new locations

--- backend/config/locations.js
+++ backend/config/locations.js
@@ -437,16 +437,16 @@ export default {
                 "year": 2012
             }
         },
-        // "_MG_2600.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2011
-        //     }
-        // },
+        "_MG_2600.webp": {
+            "location": {
+                "place": 0,
+                "x": 130,
+                "y": 227
+            },
+            "date": {
+                "year": 2011
+            }
+        },
         // "Robotics_Engineering_Ph17.webp": {
         //     "location": {
         //         "place": 0,
@@ -467,36 +467,36 @@ export default {
         //         "year": 1993
         //     }
         // },
-        // "Photo11.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1996
-        //     }
-        // },
-        // "Sagan_DC.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1991
-        //     }
-        // },
-        // "Scan_10.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2000
-        //     }
-        // },
+        "Photo11.webp": {
+            "location": {
+                "place": 0,
+                "x": 135,
+                "y": 240
+            },
+            "date": {
+                "year": 1996
+            }
+        },
+        "Sagan_DC.webp": {
+            "location": {
+                "place": 0,
+                "x": 126,
+                "y": 241
+            },
+            "date": {
+                "year": 1991
+            }
+        },
+        "Scan_10.webp": {
+            "location": {
+                "place": 0,
+                "x": 198,
+                "y": 74
+            },
+            "date": {
+                "year": 2000
+            }
+        },
         // "Scan_18.webp": {
         //     "location": {
         //         "place": 0,
@@ -507,46 +507,46 @@ export default {
         //         "year": 2000
         //     }
         // },
-        // "IMG_2841.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2016
-        //     }
-        // },
-        // "IMG_2842.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2016
-        //     }
-        // },
-        // "phone_booth_room.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2016
-        //     }
-        // },
-        // "learning_lab.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2016
-        //     }
-        // },
+        "IMG_2841.webp": {
+            "location": {
+                "place": 0,
+                "x": 159,
+                "y": 148
+            },
+            "date": {
+                "year": 2016
+            }
+        },
+        "IMG_2842.webp": {
+            "location": {
+                "place": 0,
+                "x": 164,
+                "y": 152
+            },
+            "date": {
+                "year": 2016
+            }
+        },
+        "phone_booth_room.webp": {
+            "location": {
+                "place": 0,
+                "x": 156,
+                "y": 152
+            },
+            "date": {
+                "year": 2016
+            }
+        },
+        "learning_lab.webp": {
+            "location": {
+                "place": 0,
+                "x": 154,
+                "y": 147
+            },
+            "date": {
+                "year": 2016
+            }
+        },
         // "Scan_8.webp": {
         //     "location": {
         //         "place": 0,
@@ -557,26 +557,26 @@ export default {
         //         "year": 2000
         //     }
         // },
-        // "Scan_12.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2000
-        //     }
-        // },
-        // "Scan_13.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2000
-        //     }
-        // },
+        "Scan_12.webp": {
+            "location": {
+                "place": 0,
+                "x": 202,
+                "y": 73
+            },
+            "date": {
+                "year": 2000
+            }
+        },
+        "Scan_13.webp": {
+            "location": {
+                "place": 0,
+                "x": 209,
+                "y": 89
+            },
+            "date": {
+                "year": 2000
+            }
+        },
         // "Scan_20.webp": {
         //     "location": {
         //         "place": 0,
@@ -587,56 +587,46 @@ export default {
         //         "year": 2000
         //     }
         // },
-        // "MarkCarlson.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2017
-        //     }
-        // },
-        // "Scan_21.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1986
-        //     }
-        // },
-        // "Scan_12.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1989
-        //     }
-        // },
-        // "Scan_26.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1989
-        //     }
-        // },
-        // "Scan_23.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1989
-        //     }
-        // },
+        "MarkCarlson.webp": {
+            "location": {
+                "place": 0,
+                "x": 80,
+                "y": 18
+            },
+            "date": {
+                "year": 2017
+            }
+        },
+        "Scan_21.webp": {
+            "location": {
+                "place": 0,
+                "x": 138,
+                "y": 78
+            },
+            "date": {
+                "year": 1986
+            }
+        },
+        "Scan_26.webp": {
+            "location": {
+                "place": 0,
+                "x": 109,
+                "y": 69
+            },
+            "date": {
+                "year": 1989
+            }
+        },
+        "Scan_23.webp": {
+            "location": {
+                "place": 0,
+                "x": 107,
+                "y": 66
+            },
+            "date": {
+                "year": 1989
+            }
+        },
         // "DSC_0255.webp": {
         //     "location": {
         //         "place": 0,
@@ -657,16 +647,16 @@ export default {
         //         "year": 2013
         //     }
         // },
-        // "TALENT_with_Sam_Yegan.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 2013
-        //     }
-        // },
+        "TALENT_with_Sam_Yegan.webp": {
+            "location": {
+                "place": 0,
+                "x": 27,
+                "y": 97
+            },
+            "date": {
+                "year": 2013
+            }
+        },
         // "_DX_7616.webp": {
         //     "location": {
         //         "place": 0,
@@ -687,26 +677,26 @@ export default {
         //         "year": 2004
         //     }
         // },
-        // "Grainger_LabDC.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1998
-        //     }
-        // },
-        // "Tellabs_Studio_for_Professional_DevelopmentDC.webp": {
-        //     "location": {
-        //         "place": 0,
-        //         "x": -1,
-        //         "y": -1
-        //     },
-        //     "date": {
-        //         "year": 1998
-        //     }
-        // }
+        "Grainger_LabDC.webp": {
+            "location": {
+                "place": 0,
+                "x": 60,
+                "y": 65
+            },
+            "date": {
+                "year": 1998
+            }
+        },
+        "Tellabs_Studio_for_Professional_DevelopmentDC.webp": {
+            "location": {
+                "place": 0,
+                "x": 29,
+                "y": 86
+            },
+            "date": {
+                "year": 1998
+            }
+        }
     },
     "weekly": {
         "20250130_164538.webp": {
